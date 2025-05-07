5d0337d53d8fb5df703f6b90de0ac958d31a888b
Ben <magician9457@gmail.com>
2025-05-07 17:31:17 +0800

    5月2號的課程

--- app.py
+++ app.py
@@ -1,24 +1,61 @@
 from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
-import pymysql
-from pm25 import get_pm25_data_from_mysql, update_db
+from pm25 import (
+    get_pm25_data_from_mysql,
+    update_db,
+    get_pm25_data_by_site,
+    get_all_counties,
+    get_site_by_county,
+)
 import json
+
 
 app = Flask(__name__)
 
-@app.route("/filter", methods=["GET"])
-def filter_data():
-    county = request.form.get("county", "ALL")
-    columns, datas = get_pm25_data_from_mysql()
-    df = pd.DataFrame(datas, columns=columns)
-    df1 = df.groupby("county").get_group(county).groupby("site")["pm25"].mean()
-    print(df1)
-    return {"county": county}
+
+@app.route("/pm25-county-site")
+def pm25_county_site():
+    county = request.args.get("county")
+    sites = get_site_by_county(county)
+    result = json.dumps(sites, ensure_ascii=False)
+
+    return result
 
 
-@app.route("/update_db")
-def updaye_pm25_db():
+@app.route("/pm25-site")
+def pm25_site():
+    counties = get_all_counties()
+    return render_template("pm25-site.html", counties=counties)
+
+
+@app.route("/pm25-data-site")
+def pm25_data_by_site():
+    county = request.args.get("county")
+    site = request.args.get("site")
+
+    if not county or not site:
+        result = json.dumps({"error": "縣市跟站點名稱不能為空!"}, ensure_ascii=False)
+    else:
+        columns, datas = get_pm25_data_by_site(county, site)
+        df = pd.DataFrame(datas, columns=columns)
+        # 轉換字串時間格式
+        date = df["datacreationdate"].apply(lambda x: x.strftime("%Y-%m-%d %H"))
+        data = {
+            "county": county,
+            "site": site,
+            "x_data": date.to_list(),
+            "y_data": df["pm25"].to_list(),
+        }
+
+        result = json.dumps(data, ensure_ascii=False)
+
+    return result
+
+
+# 更新資料庫
+@app.route("/update-db")
+def update_pm25_db():
     count, message = update_db()
     nowtime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     result = json.dumps(
@@ -27,72 +64,40 @@ def updaye_pm25_db():
     return result
 
 
-
 @app.route("/")
 def index():
+    # 取得資料庫最新資料
     columns, datas = get_pm25_data_from_mysql()
+    # 取出不同縣市給select
     df = pd.DataFrame(datas, columns=columns)
+    # 排序縣市
     counties = sorted(df["county"].unique().tolist())
-    print(counties)
-    
-    columns, datas = get_pm25_data_from_mysql()
-    df = pd.DataFrame(datas, columns=columns)
 
+    # 選取縣市後的資料(預設ALL)
     county = request.args.get("county", "ALL")
-    if county != "ALL":
-        df1 = df.groupby("county").get_group(county)
-        columns = df1.columns.tolist()
-        datas = df1.values.tolist()
-        print(df1)
-    return render_template("index.html", columns=columns, datas=datas, counties=counties)
 
-
-@app.route("/books")
-def books_page():
-    books = [
-        {
-            "name": "Python book",
-            "price": 299,
-            "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
-        },
-        {
-            "name": "Java book",
-            "price": 399,
-            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
-        },
-        {
-            "name": "C# book",
-            "price": 499,
-            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
-        },
-    ]
-
-    books = []
-    if books:
-        for book in books:
-            print(book["name"])
-            print(book["price"])
-            print(book["image_url"])
+    if county == "ALL":
+        df1 = df.groupby("county")["pm25"].mean().reset_index()
+        x_data = df1["county"].to_list()
     else:
-        print("販售完畢，目前無書籍!")
+        # 取得特定縣市的資料
+        df = df.groupby("county").get_group(county)
+        # 繪製所需資料
+        x_data = df["site"].to_list()
 
-    username = "jerry"
-    nowtime = datetime.now().strftime("%Y-%m-%d")
-    print(username, nowtime)
-    return render_template("books.html", name=username, now=nowtime, books=books)
+    y_data = df["pm25"].to_list()
+    columns = df.columns.tolist()
+    datas = df.values.tolist()
 
-
-@app.route("/bmi")
-def get_bmi():
-    # args=>GET
-    height = eval(request.args.get("height"))
-    weight = eval(request.args.get("weight"))
-
-    print(height, weight)
-
-    bmi = round(weight / (height / 100) ** 2, 2)
-
-    return render_template("bmi.html", **locals())
+    return render_template(
+        "index.html",
+        columns=columns,
+        datas=datas,
+        counties=counties,
+        selected_county=county,
+        x_data=x_data,
+        y_data=y_data,
+    )
 
 
 if __name__ == "__main__":
